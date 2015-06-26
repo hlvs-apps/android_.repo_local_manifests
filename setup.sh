622649aae670a81e581b9806c7333e807cf90631
tobigun <hennymcc@yahoo.de>
2015-06-27 01:22:23 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Install manifest
+cp roomservice.xml .repo/local_manifests/roomservice.xml
+
+# Grab all Samsung Galaxy Tab 4 7.0 (T235) patches
+
+# BUILD
+cd build
+
+# Recovery: remove wiping of res folder
+# TODO: remove. Already fixed by 3454ade92db48236057814f0ade5fa45bd57cd62 (uses /twres now)
+#git fetch https://github.com/omnirom/android_build.git android-5.0
+#git cherry-pick 12df78d8848325537d64ff0d0370aac6af203866
+cd -
+
+# FRAMEWORK
+cd frameworks/av
+
+# Add Samsung WFD Service -- required for HWComposer
+git fetch https://github.com/cm-3470/android_frameworks_av.git cm-12.1
+git cherry-pick d4848efbd5650a48f93a784e5de248a13b830c04
+cd -
+
+cd frameworks/native
+
+# Revert "Remove Parcel::writeIntPtr."
+git fetch https://github.com/cm-3470/android_frameworks_native.git cm-12.1
+git cherry-pick ce3244f93e8c42ba0f67b449647d2c2e2b5c2f76
+cd -
+
+cd frameworks/base
+
+# Fix volume key music controls and wake up
+#git fetch https://github.com/cm-3470/android_frameworks_base.git cm-12.1
+#git cherry-pick f92549d029b64a6a822d155cf76179f1e280bdcd
+cd -
+
